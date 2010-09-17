f97b4455a59755d5f5e0be8b54d12e698bd61f91
Bo Ferri <zazi@smiy.org>
2010-09-17 15:49:15 +0000

    cognitive_pattern:  * UPDATED: examples

--- examples/gen_xml.sh
+++ examples/gen_xml.sh
@@ -2,7 +2,7 @@
 
 # Generate the RDF/XML from the Turtle code
 
-rapper -i turtle -o rdfxml-abbrev N3/wi_-_scientific_research_interests_example.n3 > RDF/wi_-_scientific_research_interests_example.rdf
-rapper -i turtle -o rdfxml-abbrev N3/wi_-_preference_example_-_w_interest_dynamics.n3 > RDF/wi_-_preference_example_-_w_interest_dynamics.rdf
-rapper -i turtle -o rdfxml-abbrev N3/wi_-_hate_example.n3 > RDF/wi_-_hate_example.rdf
-rapper -i turtle -o rdfxml-abbrev N3/wi_-_music_preference_example.n3 > RDF/wi_-_music_preference_example.rdf
+rapper -i turtle -o rdfxml-abbrev N3/cco_-_scientific_research_interests_example.n3 > RDF/cco_-_scientific_research_interests_example.rdf
+rapper -i turtle -o rdfxml-abbrev N3/cco_-_preference_example.n3 > RDF/cco_-_preference_example.rdf
+rapper -i turtle -o rdfxml-abbrev N3/cco_-_hate_example.n3 > RDF/cco_-_hate_example.rdf
+rapper -i turtle -o rdfxml-abbrev N3/cco_-_music_preference_example.n3 > RDF/cco_-_music_preference_example.rdf
