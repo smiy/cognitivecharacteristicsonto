f825cd74663d1276a7e0ebf5ee5931e35f21da95
Bo Ferri <zazi@smiy.org>
2010-09-20 10:34:09 +0000

    cognitive_pattern:  * ADDED: cognitive pattern example

--- examples/gen_xml.sh
+++ examples/gen_xml.sh
@@ -6,3 +6,4 @@ rapper -i turtle -o rdfxml-abbrev N3/cco_-_scientific_research_interests_example
 rapper -i turtle -o rdfxml-abbrev N3/cco_-_preference_example.n3 > RDF/cco_-_preference_example.rdf
 rapper -i turtle -o rdfxml-abbrev N3/cco_-_hate_example.n3 > RDF/cco_-_hate_example.rdf
 rapper -i turtle -o rdfxml-abbrev N3/cco_-_music_preference_example.n3 > RDF/cco_-_music_preference_example.rdf
+rapper -i turtle -o rdfxml-abbrev N3/cco_-_cognitive_patterns.n3 > RDF/cco_-_cognitive_patterns.rdf
