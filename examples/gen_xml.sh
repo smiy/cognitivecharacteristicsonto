9563b278f748c79527d434173600997e4ca4e44d
Bo Ferri <zazi@smiy.org>
2010-09-22 09:16:56 +0000

    cognitive_pattern:  * FIXED: UMIRL example typo and namespaces

--- examples/gen_xml.sh
+++ examples/gen_xml.sh
@@ -7,3 +7,4 @@ rapper -i turtle -o rdfxml-abbrev N3/cco_-_preference_example.n3 > RDF/cco_-_pre
 rapper -i turtle -o rdfxml-abbrev N3/cco_-_hate_example.n3 > RDF/cco_-_hate_example.rdf
 rapper -i turtle -o rdfxml-abbrev N3/cco_-_music_preference_example.n3 > RDF/cco_-_music_preference_example.rdf
 rapper -i turtle -o rdfxml-abbrev N3/cco_-_cognitive_patterns.n3 > RDF/cco_-_cognitive_patterns.rdf
+rapper -i turtle -o rdfxml-abbrev N3/cco_-_UMIRL_example.n3 > RDF/cco_-_UMIRL_example.rdf
